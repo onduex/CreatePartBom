2f07b135b3c37b365cb6f350fc987b7505edc5a8
TOMAS PASCUAL <tomas.pascual@onduex.com>
2023-04-04 17:50:38 +0200

    [IMP] Get Items Ready to be Created

--- CreatePartBom/EventHandlers.vb
+++ CreatePartBom/EventHandlers.vb
@@ -1,5 +1,7 @@
 Imports Autodesk.Connectivity.Extensibility.Framework
 Imports Autodesk.Connectivity.WebServices
+Imports ACW = Autodesk.Connectivity.WebServices
+Imports ACWT = Autodesk.Connectivity.WebServicesTools
 
 <Assembly: ApiVersion("16.0")>
 <Assembly: ExtensionId("89facae7-a876-4ce5-8d9f-a20666971e21")>
@@ -12,16 +14,17 @@ Public Class EventHandlers
 
     Public Sub OnLoad() Implements IWebServiceExtension.OnLoad
         ' Item Events
-        AddHandler ItemService.PromoteItemEvents.GetRestrictions, AddressOf PromoteItemEvents_GetRestrictions
-        ' AddHandler ItemService.PromoteItemEvents.Post, AddressOf PromoteItemEvents_Post
+        AddHandler ItemService.PromoteItemEvents.Post, AddressOf PromoteItemEvents_Post
     End Sub
 
 #End Region
 
-    Private Sub PromoteItemEvents_GetRestrictions(ByVal sender As Object, ByVal e As PromoteItemCommandEventArgs)
-        MsgBox("Hola Rafa, encabronados con el Vault ahora " & e.Status.ToString)
-        e.AddRestriction(New ExtensionRestriction(e.FileIds(0).ToString, "Test restriction"))
+    Private Sub PromoteItemEvents_Post(ByVal sender As Object, ByVal e As PromoteItemCommandEventArgs)
+        Dim mFileId As Long = e.FileIds(0)
+        Dim mItemService As Autodesk.Connectivity.WebServices.ItemService = sender
+        Dim mItem As GetPromoteOrderResults = mItemService.GetPromoteComponentOrder(timestamp:=Now)
+        Debug.Print(mItem.PrimaryArray(0))
+        Debug.Print("Eureka")
     End Sub
 
 End Class
-
