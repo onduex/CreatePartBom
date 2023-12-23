c46a46f4aa6427afbada9ee45a4e9036dec96ae9
Tomás Pascual <tomas.pascual@onduex.com>
2023-12-24 00:42:02 +0100

    [IMP] printing all properties id, name and value in debug.print

--- CreatePartBom/EventHandlers.vb
+++ CreatePartBom/EventHandlers.vb
@@ -3,9 +3,8 @@ Imports Autodesk.Connectivity.WebServices
 Imports ACW = Autodesk.Connectivity.WebServices
 Imports ACWT = Autodesk.Connectivity.WebServicesTools
 
-<Assembly: ApiVersion("16.0")>
+<Assembly: ApiVersion("17.0")>
 <Assembly: ExtensionId("89facae7-a876-4ce5-8d9f-a20666971e21")>
-
 
 Public Class EventHandlers
     Implements IWebServiceExtension
@@ -20,11 +19,39 @@ Public Class EventHandlers
 #End Region
 
     Private Sub PromoteItemEvents_Post(ByVal sender As Object, ByVal e As PromoteItemCommandEventArgs)
-        Dim mFileId As Long = e.FileIds(0)
+        Dim mFileId As Long
         Dim mItemService As Autodesk.Connectivity.WebServices.ItemService = sender
-        Dim mItem As GetPromoteOrderResults = mItemService.GetPromoteComponentOrder(timestamp:=Now)
-        Debug.Print(mItem.PrimaryArray(0))
-        Debug.Print("Eureka")
+        Dim mDocumentService As Autodesk.Connectivity.WebServices.DocumentService = mItemService.WebServiceManager.DocumentService
+        Dim mPropertyService As Autodesk.Connectivity.WebServices.PropertyService = mItemService.WebServiceManager.PropertyService
+        Dim mItemIds As GetPromoteOrderResults = mItemService.GetPromoteComponentOrder(timestamp:=Now)
+        Dim mFile As File
+        Dim mFileProperties As PropInst()
+        Dim mProperty As PropInst
+        Dim mPropertyDefinition As PropDefInfo()
+
+        For Each mFileId In e.FileIds
+            For Each mItemId In mItemIds.PrimaryArray()
+                mFile = mDocumentService.GetFileById(mFileId)
+                mFileProperties = mPropertyService.GetPropertiesByEntityIds("FILE", New Long() {mFile.Id})
+                mPropertyDefinition = mPropertyService.GetPropertyDefinitionInfosByEntityClassId("FILE", New Long() {110})
+
+                ' Dim itemProperties As PropInst() = propSvc.GetPropertiesByEntityIds("ITEM", New Long() {Item.Id})
+
+                Debug.Print("El fichero: " & mFile.Name &
+                            " con Id: " & mFileId &
+                            " se va a promocionar al Item: " & mItemId)
+                Debug.Print("##################################################################################################")
+
+                For Each mProperty In mFileProperties
+                    mPropertyDefinition = mPropertyService.GetPropertyDefinitionInfosByEntityClassId("FILE", New Long() {mProperty.PropDefId})
+                    If mPropertyDefinition(0).PropDef.DispName <> "Miniatura" Then
+                        Debug.Print(mPropertyDefinition(0).PropDef.Id &
+                                    " : " & mPropertyDefinition(0).PropDef.DispName &
+                                    " = " & mProperty.Val)
+                    End If
+                Next
+            Next
+        Next
     End Sub
 
 End Class
